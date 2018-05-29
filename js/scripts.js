a704fb0c096ae084f3ff874db97da62ea5e2febf
Brian Palowski and Eliot Charette <Guest@epicodus-57.local>
2018-05-29 11:29:08 -0700

    added different functions to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,54 @@
+/*
 var add = function(number1, number2) {
   return number1 + number2;
 };
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(result);
 
-alert(add(number1, number2));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = subtract(number1, number2);
+alert(result);
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = multiply(number1, number2);
+alert(result);
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = divide(number1, number2);
+alert(result);
+
+var bmi = function(number1, number2) {
+  return number1 / number2 / number2;
+};
+
+var number1 = parseInt(prompt("Enter your weight in kilos:"));
+var number2 = parseInt(prompt("Enter your height in meters:"));
+var result = bmi(number1, number2);
+alert(result);
+*/
+
+var temp = function(number1) {
+  return number1 * 1.8 + 32;
+};
+
+var number1 = parseInt(prompt("Enter temp in celcius:"));
+var result = temp(number1);
+alert(result);
