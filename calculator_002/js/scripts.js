ea7a802a0c29a553624de7b54285cfc76fcc9384
Brian <39497182+skwad01@users.noreply.github.com>
2018-05-30 01:02:37 -0700

    Add files via upload
    Uploaded for flash Card

---
+++ calculator_002/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+$(".clickable").click(function(){
+  $("#js-on").toggle();
+  $("#js-off").toggle();
+});
+  $(".clickable_01").click(function(){
+  $("#operators-on").toggle();
+  $("#operators-off").toggle();
+
+});
+});
